988fb4d9022557574e6392b4b03abb18644f83d7
Houndoom <therealhoundoom@gmail.com>
2022-08-14 20:18:47 +0800

    Implemented quantity input and increase/decrease

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import pokedollar from '../images/pokedollar.png';
 
 const Card = (props) => {
+
+  const [quantity, setQuantity] = useState(0);
+
+  const addQuantity = () => {
+    if (quantity < 99) setQuantity(i => i + 1);
+  }
+
+  const minusQuantity = () => {
+    if (quantity > 0) setQuantity(i => i - 1);
+  }
+
+  const changeQuantity = (e) => {
+    setQuantity(parseInt(e.target.value));
+  }
+
   return (
     <div className="card">
       <img src={props.img} alt={props.name} />
       <div className="name">{props.name}</div>
       <div className="price">
         <img src={pokedollar} alt="pokedollars" class='pokedollar' />{props.price}
+      </div>
+      <div className="quantity">
+        <div onClick={minusQuantity}>–</div>
+        <input type="text" pattern="\d*" maxlength="2" aria-label={props.name} value={quantity} onChange={changeQuantity} />
+        <div onClick={addQuantity}>+</div>
       </div>
     </div>
   )
--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { screen, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import userEvent from '@testing-library/user-event';
 import Header from '../Header';
 import { BrowserRouter as Router } from 'react-router-dom';
 
