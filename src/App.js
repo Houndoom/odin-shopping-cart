e1a5284ff53ff6b67b23aed450d519bdcf470a44
Houndoom <therealhoundoom@gmail.com>
2022-08-16 23:15:25 +0800

    Change BrowserRouter to HashRouter which is compatible with gh-pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Header from './components/Header';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import About from './components/About';
 import Shop from './components/Shop';
 import Cart from './components/Cart';
